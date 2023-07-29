b06530831a1929452b9992d1b3d62360b7fe2a77
Jacques Drumond <jacques.junio@hotmail.com>
2023-07-29 10:37:55 -0300

    forms validation and styling

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,4 +1,6 @@
 from django import forms
 
 class ReviewForm(forms.Form):
-    username = forms.CharField(max_length= 100, required=False)
+    username = forms.CharField(max_length= 100, required=True, min_length=10, error_messages={
+        "required": "Username can't be blank."
+    })
