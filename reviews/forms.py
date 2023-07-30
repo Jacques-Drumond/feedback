235a4c4d77aa068dca585f9c59031059b5faf825
Jacques Drumond <jacques.junio@hotmail.com>
2023-07-30 17:32:40 -0300

    creating models

--- reviews/forms.py
+++ reviews/forms.py
@@ -4,3 +4,5 @@ class ReviewForm(forms.Form):
     username = forms.CharField(max_length= 100, required=True, min_length=10, error_messages={
         "required": "Username can't be blank."
     })
+    review_text = forms.CharField(label="Feedback", widget=forms.Textarea, max_length=300, min_length=10)
+    rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
