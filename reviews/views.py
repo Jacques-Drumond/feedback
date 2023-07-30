d3afe31a974d84796d060bca131c4781b00760d8
Jacques Drumond <jacques.junio@hotmail.com>
2023-07-30 11:33:53 -0300

    adding comments

--- reviews/views.py
+++ reviews/views.py
@@ -1,19 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import ReviewForm
+
 def index(request):
-    if request.method == "POST": 
+    # se a requisição for POST. 
+    if request.method == "POST":
+        # instancia o formulário com os dados da requisição.
         form = ReviewForm(request.POST)
+        
+        # verifica se o formulário é valido (parâmetros configurados no forms).
         if form.is_valid():
             print(form.cleaned_data)
+            # redireciona para a pagina /thank-you fazendo com que a função thank_you rode.
             return HttpResponseRedirect('/thank-you')
+        
+    # se a requisição não for POST
     else:
+        # instancia o form sem dados, só a base, o cód HTML etc.
         form = ReviewForm()
 
+    # retorna a pagina /review com a base do formulário.
     return render(request, 'reviews/review.html', {
         "form": form
     })
 
-
+# retorna a pag /thank-you
 def thank_you(request):
     return render(request, 'reviews/thankyou.html', )
