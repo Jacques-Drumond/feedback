8e72536ff5d6277824bab941b8a35b3273d6792b
Jacques Drumond <jacques.drumond@matrix.local>
2023-08-02 16:29:45 -0300

    a

--- reviews/views.py
+++ reviews/views.py
@@ -1,32 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import ReviewForm
-def index(request):
-    # se a requisição for POST. 
-    if request.method == "POST":
-        # instancia o formulário com os dados da requisição.
-        form = ReviewForm(request.POST)
+from django.views import View
+
+class ReviewView(View):
+    def get(self, request):
         
-        # verifica se o formulário é valido (parâmetros configurados no forms).
+        form = ReviewForm()
+
+        return render(request, 'reviews/review.html', {
+            "form": form
+        })
+    
+    def post(self, request):
+
+        form = ReviewForm(request.POST)
+
         if form.is_valid():
             print(form.cleaned_data)
             form.save()
-            # instancia na variável os dados coletados do formulário, as keys vem do 
-            # models user_name, review etc.
+            return HttpResponseRedirect("/thank-you")
 
-            # utiliza o método de modelo save() para salvar no database
-            # redireciona para a pagina /thank-you fazendo com que a função thank_you rode.
-            return HttpResponseRedirect('/thank-you')
-        
-    # se a requisição não for POST
-    else:
-        # instancia o form sem dados, só a base, o cód HTML etc.
-        form = ReviewForm()
+        return render(request, 'reviews/review.html', {
+            "form": form
+        })
 
-    # retorna a pagina /review com a base do formulário.
-    return render(request, 'reviews/review.html', {
-        "form": form
-    })
 
 # retorna a pag /thank-you
 def thank_you(request):
