ee1c04eb7749125561c84dcc72cf4c72df220779
Jacques Drumond <jacques.drumond@matrix.local>
2023-08-08 12:53:28 -0300

    a

--- profiles/views.py
+++ profiles/views.py
@@ -5,6 +5,7 @@ import os
 from .functions import get_folder_size, store_file
 from .forms import profileForm
 from .models import UserProfile
+from django.views.generic import ListView
 from django.views.generic.edit import CreateView
 # Create your views here.
 
@@ -13,3 +14,9 @@ class CreateProfileView(CreateView):
     model = UserProfile
     fields = "__all__"
     success_url = "/profiles"
+
+
+class ProfilesView(ListView):
+    model = UserProfile
+    template_name = "profiles/user_profiles.html"
+    context_object_name = "profiles"
