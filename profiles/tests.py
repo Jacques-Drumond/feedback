39436690a826a3d604f213691d502490def026bd
Jacques Drumond <jacques.junio@hotmail.com>
2023-08-07 21:59:10 -0300

    adding folder limit'

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+import os
+
+def get_folder_size(folder_path):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(folder_path):
+        for filename in filenames:
+            file_path = os.path.join(dirpath, filename)
+            total_size += os.path.getsize(file_path)
+    return total_size
--- profiles/views.py
+++ profiles/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
+import os
+from .functions import get_folder_size
 # Create your views here.
 
-
-def store_file(file):
-    with open(f"temp/{file.name}.jpg", "wb+") as dest:
+def store_file(folder, file):
+    with open(f"{folder}{file.name}.jpg", "wb+") as dest:
         for chunk in file.chunks():
             dest.write(chunk)
-
 
 class CreateProfileView(View):
     def get(self, request):
         return render(request, "profiles/create_profile.html")
 
     def post(self, request):
-        store_file(request.FILES['image'])
+        if get_folder_size("temp") < 4501795:
+            store_file("temp/", request.FILES['image'])
+        else:
+            store_file("temp2/", request.FILES['image'])
         return HttpResponseRedirect("/profiles")
