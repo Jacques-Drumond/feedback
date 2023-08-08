dfded62be85b21580ba0acb304a9150cd9354478
Jacques Drumond <jacques.drumond@matrix.local>
2023-08-08 12:10:43 -0300

    a

--- profiles/views.py
+++ profiles/views.py
@@ -5,25 +5,11 @@ import os
 from .functions import get_folder_size, store_file
 from .forms import profileForm
 from .models import UserProfile
+from django.views.generic.edit import CreateView
 # Create your views here.
 
-class CreateProfileView(View):
-
-    def get(self, request):
-        form = profileForm()
-        return render(request, "profiles/create_profile.html", {
-            "form": form
-        })
-    
-    def post(self, request):
-
-        submitted_form = profileForm(request.POST, request.FILES)
-
-        if submitted_form.is_valid():
-            profile = UserProfile(image=request.FILES['user_image'])
-            profile.save()
-            return HttpResponseRedirect("/profiles")
-        else:
-            return render(request, "profiles/create_profile.html", {
-                "form": submitted_form
-            })
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
