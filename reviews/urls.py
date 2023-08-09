6065a3dc69f0ed4f46ba05a0812afc9307277eca
Jacques Drumond <jacques.drumond@matrix.local>
2023-08-09 10:15:54 -0300

    a

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.ReviewView.as_view()),
     path('thank-you', views.thankYouView.as_view()),
     path('review-list', views.reviewsListView.as_view()),
+    path('reviews/favorite', views.AddFavoriteView.as_view()),
     path('reviews/<int:pk>', views.reviewDetail.as_view())
 ]
     
